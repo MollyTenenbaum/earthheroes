d1fb93697d58a3fa2d6bcd0b482f70e8ceb7e7a6
Molly Tenenbaum <mollytenenbaum100@gmail.com>
2020-07-29 11:06:41 -0700

    Initial Commit

---
+++ earthheroes/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  earthheroes
+//
+//  Created by Molly Tenenbaum on 7/29/20.
+//  Copyright © 2020 Molly Tenenbaum. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ earthheroes/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  earthheroes
+//
+//  Created by Molly Tenenbaum on 7/29/20.
+//  Copyright © 2020 Molly Tenenbaum. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
